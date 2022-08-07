3729319752c0744be84af87e723710cbc363d7d1
NaHyeon <nahyeonee99@gmail.com>
2022-08-07 16:45:23 +0900

    initial commit

---
+++ src/main/java/com/cafehub/cafehubspring/CafehubSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cafehub.cafehubspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafehubSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafehubSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafehub/cafehubspring/CafehubSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafehub.cafehubspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafehubSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
