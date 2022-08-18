f58c5b9b1bbe8e0c2cc3ca21bce237fe569eb8ac
nahyeon99 <nahyeon99@gmail.com>
2022-08-18 15:36:07 +0900

    [Feat] Swagger 띄우기 - #2

--- src/main/java/com/cafehub/cafehubspring/config/SwaggerConfiguration.java
+++ src/main/java/com/cafehub/cafehubspring/config/SwaggerConfiguration.java
@@ -1,25 +1,13 @@
 package com.cafehub.cafehubspring.config;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * http://localhost:8080/swagger-ui/#/
@@ -33,47 +21,15 @@ public class SwaggerConfiguration {
      */
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.OAS_30)
-                .alternateTypeRules(AlternateTypeRules
-                        .newRule(Pageable.class, Page.class))
                 .useDefaultResponseMessages(false) // Swagger에서 제공해주는 기본 응답 코드 (200, 401, 403, 404). false로 설정하면 기본 응답 코드를 노출하지 않습니다.
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.baeldung.swaggerconf.controller")) // Basic error controller 를 없애기 위한 코드
                 .apis(RequestHandlerSelectors.any()) // Swagger를 적용할 패키지 설정
                 .paths(PathSelectors.any()) // Swagger를 적용할 주소 패턴을 세팅
-                .build()
-                .apiInfo(apiInfo()).securityContexts(Arrays.asList(securityContext())) // Swagger UI로 노출할 정보
-                .securitySchemes(Arrays.asList(apiKey()));
+                .build();
     }
-
-    /**
-     * jwt를 통해서 인증/인가 설정을 했을 시, swagger에서도 해당 설정을 잡아주는 부분
-     */
-    private ApiKey apiKey() {
-
-        return new ApiKey("X-AUTH-TOKEN", "X-AUTH-TOKEN", "header");
-    }
-
-    private SecurityContext securityContext() {
-        return springfox
-                .documentation
-                .spi.service
-                .contexts
-                .SecurityContext
-                .builder()
-                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("X-AUTH-TOKEN", authorizationScopes));
-    }
-
-    /**
-     * jwt 인증을 위한 swagger 코드 end
-     */
 
     /**
      * api 정보 설정 부분
@@ -85,15 +41,4 @@ public class SwaggerConfiguration {
                 .version("1.0")
                 .build();
     }
-
-    /**
-     * Page 파라미터를 위한 Page Class
-     */
-    @Data
-    @ApiModel
-    static class Page{
-        @ApiModelProperty(value = "페이지번호")
-        private Integer page;
-    }
-
 }
