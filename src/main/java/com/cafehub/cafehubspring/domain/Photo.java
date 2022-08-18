a7aed12d19a35ec514f555696901a84780a60e0c
gs97ahn <gs97ahn@naver.com>
2022-08-18 16:38:30 +0900

    [Feat] Photo Entity, Photo Repository 구현 -#4

--- src/main/java/com/cafehub/cafehubspring/domain/Photo.java
+++ src/main/java/com/cafehub/cafehubspring/domain/Photo.java
@@ -1,4 +1,41 @@
 package com.cafehub.cafehubspring.domain;
 
-public class Photo {
+import com.cafehub.cafehubspring.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Photo extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "photo_id")
+    private String id;
+
+    private String fileName;
+
+    private String url;
+
+    @Builder
+    public Photo(String fileName, String url) {
+        this.fileName = fileName;
+        this.url = url;
+    }
+
+    public void updateFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void updateUrl(String url) {
+        this.url = url;
+    }
 }
