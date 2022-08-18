e9f8cd9212e7412c8e9da59e3b4220576fca9f94
gs97ahn <gs97ahn@naver.com>
2022-08-18 15:54:22 +0900

    [Fix] Id annotation 수정 - #17

--- src/main/java/com/cafehub/cafehubspring/common/BaseTimeEntity.java
+++ src/main/java/com/cafehub/cafehubspring/common/BaseTimeEntity.java
@@ -2,12 +2,10 @@ package com.cafehub.cafehubspring.common;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -15,6 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreatedDate
