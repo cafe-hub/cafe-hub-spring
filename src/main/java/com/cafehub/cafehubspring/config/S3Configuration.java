b069f3574de64bd2d1251b32bc2c29f068f83148
gs97ahn <gs97ahn@naver.com>
2022-08-19 14:37:16 +0900

    Revert "[Fix] amazonS3를 amazonS3Client로 변경 - #26"
    This reverts commit 3ca0f0afb761fc5a86dc534dcea14253745869f2.

--- src/main/java/com/cafehub/cafehubspring/config/S3Configuration.java
+++ src/main/java/com/cafehub/cafehubspring/config/S3Configuration.java
@@ -4,7 +4,6 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,9 +22,9 @@ public class S3Configuration {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
