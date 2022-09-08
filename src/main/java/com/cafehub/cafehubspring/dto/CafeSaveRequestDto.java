2714455c9e4021dc6f61ef7d276d4adf419e34bb
nahyeon99 <nahyeonee99@gmail.com>
2022-09-08 12:15:15 +0900

    [Style] 주석 제거, 필요없는 코드 제거

--- src/main/java/com/cafehub/cafehubspring/dto/CafeSaveRequestDto.java
+++ src/main/java/com/cafehub/cafehubspring/dto/CafeSaveRequestDto.java
@@ -1,6 +1,5 @@
 package com.cafehub.cafehubspring.dto;
 
-import com.cafehub.cafehubspring.domain.Cafe;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -26,59 +25,17 @@ public class CafeSaveRequestDto {
     @Size(min = 5, max = 20, message = "주소는 5~20자만 가능합니다")
     private String location;
 
-/*    @ApiModelProperty(position = 3, required = true, value = "영업시간 월")
-    private String openingHoursMon;
-
-    @ApiModelProperty(position = 4, required = true, value = "영업시간 화")
-    private String openingHoursTues;
-
-    @ApiModelProperty(position = 5, required = true, value = "영업시간 수")
-    private String openingHoursWed;
-
-    @ApiModelProperty(position = 6, required = true, value = "영업시간 목")
-    private String openingHoursThurs;
-
-    @ApiModelProperty(position = 7, required = true, value = "영업시간 금")
-    private String openingHoursFri;
-
-    @ApiModelProperty(position = 8, required = true, value = "영업시간 토")
-    private String openingHoursSat;
-
-    @ApiModelProperty(position = 9, required = true, value = "영업시간 일")
-    private String openingHoursSun;*/
-
-    @ApiModelProperty(position = 10, required = true, value = "사진 URL")
+    @ApiModelProperty(position = 3, required = true, value = "사진 URL")
     private List<String> photoUrl;
 
-    @ApiModelProperty(position = 11,
+    @ApiModelProperty(position = 4,
             dataType = "String",
             value = "콘센트상태",
             allowableValues = "null, many",
             example = "null")
     private String plugStatus;
 
-    @ApiModelProperty(position = 12, dataType = "List<MultiPartFile>", value = "사진")
+    @ApiModelProperty(position = 5, dataType = "List<MultiPartFile>", value = "사진")
     private List<MultipartFile> files;
 
-    public Cafe toCafeEntity(Float latitude, Float longitude) {
-        return Cafe.builder()
-                .cafeName(cafeName)
-                .location(location)
-                .latitude(latitude)
-                .longitude(longitude)
-                .plugStatus(plugStatus)
-                .build();
-    }
-
-//    public OpeningHours toOpeningHoursEntity(String mon, String tues, String wed, String thu, String fri, String sat, String sun) {
-//        return OpeningHours.builder()
-//                .monday(mon)
-//                .tuesday(tues)
-//                .wednesday(wed)
-//                .thursday(thu)
-//                .friday(fri)
-//                .saturday(sat)
-//                .sunday(sun)
-//                .build();
-//    }
 }
--- src/main/java/com/cafehub/cafehubspring/repository/CafeRepository.java
+++ src/main/java/com/cafehub/cafehubspring/repository/CafeRepository.java
@@ -6,11 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
-
-    Optional<Cafe> findByCafeName(String cafeName);
 
     @Query(value = "SELECT c FROM Cafe c WHERE c.latitude < :topLeftLatitude AND c.latitude > :bottomRightLatitude " +
                     "AND c.longitude > :topLeftLongitude AND c.longitude < :bottomRightLongitude",
