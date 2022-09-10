30ad52ffe49235e40daa9d1b0c088d3b930d2b77
nahyeon99 <nahyeonee99@gmail.com>
2022-09-10 12:03:44 +0900

    [Fix] CafeRepository Double로 형변환 - #39

--- src/main/java/com/cafehub/cafehubspring/repository/CafeRepository.java
+++ src/main/java/com/cafehub/cafehubspring/repository/CafeRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
-    @Query(value = "SELECT c FROM Cafe c WHERE c.latitude < :topLeftLatitude AND c.latitude > :bottomRightLatitude " +
+    @Query(value = "SELECT * FROM Cafe c WHERE c.latitude < :topLeftLatitude AND c.latitude > :bottomRightLatitude " +
                     "AND c.longitude > :topLeftLongitude AND c.longitude < :bottomRightLongitude",
             nativeQuery = true)
-    List<Cafe> findCafesByCoordinates(@Param("topLeftLatitude") Float topLeftLatitude,
-                         @Param("bottomRightLatitude") Float bottomRightLatitude,
-                         @Param("topLeftLongitude") Float topLeftLongitude,
-                         @Param("bottomRightLongitude") Float bottomRightLongitude);
+    List<Cafe> findCafesByCoordinates(@Param("topLeftLatitude") Double topLeftLatitude,
+                         @Param("bottomRightLatitude") Double bottomRightLatitude,
+                         @Param("topLeftLongitude") Double topLeftLongitude,
+                         @Param("bottomRightLongitude") Double bottomRightLongitude);
 }
